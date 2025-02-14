310cc5fa8911c0236ff06bf7aa574d4a6ecd0b67
Fabien Heilles <phabi1@hotmail.fr>
2025-02-14 15:22:12 +0100

    fix(apps-client-web): responsive

--- apps/client/web/src/game/scenes/preload.scene.ts
+++ apps/client/web/src/game/scenes/preload.scene.ts
@@ -6,7 +6,68 @@ export class PreloadScene extends Scene {
   }
 
   preload() {
-    this.load.image('logo', 'assets/images/logo.png');
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        color: '#ffffff',
+      },
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    const percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        color: '#ffffff',
+      },
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    const assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+        font: '18px monospace',
+        color: '#ffffff',
+      },
+    });
+    assetText.setOrigin(0.5, 0.5);
+
+    this.load.on('progress', function (value: number) {
+      percentText.setText(value * 100 + '%');
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+    });
+
+    this.load.on('fileprogress', function (file: any) {
+      assetText.setText('Loading asset: ' + file.key);
+    });
+    this.load.on('complete', function () {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+    });
+
+    this.load.image('logo', 'zenvalogo.png');
+    for (let i = 0; i < 5000; i++) {
+      this.load.image('logo' + i, 'zenvalogo.png');
+    }
   }
 
   create() {
--- apps/client/web/src/game/setup.ts
+++ apps/client/web/src/game/setup.ts
@@ -1,14 +1,33 @@
 import {} from 'phaser';
 import { PreloadScene } from './scenes/preload.scene';
 import { GameScene } from './scenes/game.scene';
-export function setup (container: string) {
-    const config = {
-        type: Phaser.AUTO,
-        parent: container,
-        width: 800,
-        height: 600,
-        scene: [PreloadScene, GameScene]
-    }
+export function setup(container: string) {
+  const config = {
+    type: Phaser.AUTO,
+    parent: container,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    scene: [PreloadScene, GameScene],
+    scale: {
+      mode: Phaser.Scale.RESIZE,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+  };
 
-    return new Phaser.Game(config);
+  const game = new Phaser.Game(config);
+
+  const onChangeScreen = () => {
+    game.scale.resize(window.innerWidth, window.innerHeight);
+  };
+
+  const _orientation =
+    screen.orientation ||
+    (screen as any).mozOrientation ||
+    (screen as any).msOrientation;
+
+  _orientation.addEventListener('change', () => onChangeScreen());
+
+  window.addEventListener('resize', () => onChangeScreen());
+
+  return game;
 }
